098ca5066d614a4f78d41882fd3262a70c4b5250
Rebecka Kinn <rebeckaki@getacademy.no>
2026-05-19 14:48:38 +0200

    Update Program.cs

--- CalcAge.API/Program.cs
+++ CalcAge.API/Program.cs
@@ -1,13 +1,11 @@
-using core;
-
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseHttpsRedirection();
-app.MapGet("/calcage", (CalcAge calcAge, bool hadBirthday) =>
-{
-    return calcAge.returnCurrentAge(hadBirthday);
-});
-app.MapPut("/saveinfo", (CalcAge calcAge, int birthYear) =>
+//app.MapGet("/calcage", (core.CalcAge calcAge, bool hadBirthday) =>
+//{
+//    return calcAge.returnCurrentAge(hadBirthday);
+//});
+app.MapPut("/saveinfo", (core.CalcAge calcAge, int birthYear) =>
 {
     calcAge.UpdateYear(birthYear);
 });
@@ -19,3 +17,8 @@ app.Run();
     CalcAge calcAge = new CalcAge(birthYear);
 });
  */
+app.MapGet("/calcage", (int birthYear, bool hadBirthday) =>
+{
+    var calcAge = new core.CalcAge(birthYear);
+    return calcAge.returnCurrentAge(hadBirthday);
+});
